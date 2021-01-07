380f83267c7ebc9155ed0dc033ca49d96346fd19
Phil Wolstenholme <philgw@gmail.com>
2021-01-07 00:51:07 +0000

    Run Colcade on idle

--- src/includes/pw-tweets.vue
+++ src/includes/pw-tweets.vue
@@ -23,12 +23,14 @@ export default {
     x-data="{}"
     x-init="() => {
       if (window.innerWidth > 767) {
-        loadjs(['https://unpkg.com/colcade@0/colcade.js'], 'colcade');
+        requestIdleCallback(() => {
+          loadjs(['https://unpkg.com/colcade@0/colcade.js'], 'colcade');
 
-        loadjs.ready('colcade', function() {
-          var colcade = new Colcade( '.tweets', {
-            columns: '.tweets__col',
-            items: '.tweets__item'
+          loadjs.ready('colcade', function() {
+            var colcade = new Colcade( '.tweets', {
+              columns: '.tweets__col',
+              items: '.tweets__item'
+            });
           });
         });
       }
