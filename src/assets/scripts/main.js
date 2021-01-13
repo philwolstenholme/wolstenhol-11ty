b8e552d0428ea9f43ee0e60e561df959e717c006
Phil Wolstenholme <philgw@gmail.com>
2021-01-13 00:58:02 +0000

    Switch away from Skypack to see if it improves blocking time

--- src/assets/scripts/main.js
+++ src/assets/scripts/main.js
@@ -1,10 +1,8 @@
 import 'alpinejs';
-// import 'wicg-inert';
-// import 'what-input';
+import 'wicg-inert';
+import 'what-input';
 import Horizon from '@mintuz/horizon';
 window.Horizon = Horizon;
-
-// quicklink
 
 if ('serviceWorker' in navigator) {
   window.addEventListener('load', () => {
