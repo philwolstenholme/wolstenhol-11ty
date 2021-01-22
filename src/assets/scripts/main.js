16803574d4f400dfff46e8cdc4ce27d61a23df18
Phil Wolstenholme <philgw@gmail.com>
2021-01-22 00:04:36 +0000

    Move header JS to file

--- src/assets/scripts/main.js
+++ src/assets/scripts/main.js
@@ -210,3 +210,29 @@ window.PwTweets = () => {
     },
   };
 };
+
+window.PwHeader = () => {
+  return {
+    active: null,
+    init() {
+      let headingsInView = [];
+      const io = new IntersectionObserver(
+        entries => {
+          const exitedElements = entries.filter(entry => !entry.isIntersecting).map(entry => entry.target);
+          const enteredElements = entries.filter(entry => entry.isIntersecting).map(entry => entry.target);
+          headingsInView = headingsInView.filter(h => !exitedElements.includes(h));
+          headingsInView.push(...enteredElements);
+          if (headingsInView.length > 0) {
+            const navigationApp = document.querySelector('header nav').__x;
+            const sectionId = (this.active = headingsInView[0].dataset.section);
+            navigationApp.$data.active = sectionId;
+          }
+        },
+        {
+          rootMargin: '-120px 0px 0px 0px',
+        }
+      );
+      document.querySelectorAll('h2').forEach(heading => io.observe(heading));
+    },
+  };
+};
