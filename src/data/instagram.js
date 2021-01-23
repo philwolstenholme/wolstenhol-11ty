385b214d0b261c6862a3352f4111c795b027be98
Phil Wolstenholme <philgw@gmail.com>
2021-01-23 00:17:02 +0000

    See if I can use my Instagram cookie to let Netlify builds work

--- src/data/instagram.js
+++ src/data/instagram.js
@@ -1,3 +1,4 @@
+require('dotenv').config();
 const Cache = require('@11ty/eleventy-cache-assets');
 const cloudinary = require('cloudinary').v2;
 const fetchBase64 = require('fetch-base64');
@@ -10,6 +11,25 @@ module.exports = async function () {
     {
       duration: '1d',
       type: 'json',
+      fetchOptions: {
+        headers: {
+          accept:
+            'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
+          'accept-language': 'en-US,en-GB;q=0.9,en;q=0.8',
+          'cache-control': 'no-cache',
+          pragma: 'no-cache',
+          'sec-fetch-dest': 'document',
+          'sec-fetch-mode': 'navigate',
+          'sec-fetch-site': 'none',
+          'sec-fetch-user': '?1',
+          'upgrade-insecure-requests': '1',
+          cookie: process.env.INSTAGRAM_COOKIE,
+        },
+        referrerPolicy: 'strict-origin-when-cross-origin',
+        body: null,
+        method: 'GET',
+        mode: 'cors',
+      },
     }
   );
 
