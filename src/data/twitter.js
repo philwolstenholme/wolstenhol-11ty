89061a020e2f2c9819a53c8e02793504b693b649
Phil Wolstenholme <philgw@gmail.com>
2021-02-03 19:28:35 +0000

    Use real Twitter data

---
+++ src/data/twitter.js
@@ -0,0 +1,19 @@
+require('dotenv').config();
+const Cache = require('@11ty/eleventy-cache-assets');
+
+module.exports = async function () {
+  let response = await Cache(
+    'https://api.twitter.com/1.1/statuses/user_timeline.json?screen_name=philw_&include_rts=true&exclude_replies=true&tweet_mode=extended&count=40&include_ext_alt_text=true',
+    {
+      duration: '1h',
+      type: 'json',
+      fetchOptions: {
+        headers: {
+          Authorization: process.env.TWITTER_AUTH_BEARER,
+        },
+      },
+    }
+  );
+
+  return response;
+};
