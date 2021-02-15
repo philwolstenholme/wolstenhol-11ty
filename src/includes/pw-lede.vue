76eebeca1e7912836632d627e6252301bba49e1d
Phil Wolstenholme <philgw@gmail.com>
2021-02-15 19:59:48 +0000

    Remove text-black from lede

--- src/includes/pw-lede.vue
+++ src/includes/pw-lede.vue
@@ -1,5 +1,5 @@
 <template>
-  <p class="font-serif text-black tracking-widest leading-relaxed max-w-3xl">
+  <p class="font-serif tracking-widest leading-relaxed max-w-3xl">
     <slot />
   </p>
 </template>
