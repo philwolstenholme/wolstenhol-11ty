75420d8884eab519462ff534e977e8bd5b5d0298
Phil Wolstenholme <philgw@gmail.com>
2021-06-25 15:06:54 +0100

    Remove scale on hover/focus-within from Twitter cards

--- src/includes/pw-card-twitter.vue
+++ src/includes/pw-card-twitter.vue
@@ -159,11 +159,6 @@ export default {
 .card__twitter {
   background-color: #1876be;
   transition: all 150ms ease-in-out;
-
-  &:hover,
-  &:focus-within {
-    transform: scale(1.005);
-  }
 }
 
 .card__twitter__tweet-text {
