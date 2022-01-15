23340a324629a7902f2043c0b5dff6a2272eed89
Phil Wolstenholme <philgw@gmail.com>
2022-01-15 00:07:33 +0000

    Add email check to function

--- src/functions/submit-reading-item/index.js
+++ src/functions/submit-reading-item/index.js
@@ -1,13 +1,21 @@
 exports.handler = async function (event, context) {
-  const { identity, user } = context.clientContext;
+  const { user } = context.clientContext;
+  const { title, url } = event.queryStringParameters;
+  const itsMe = user.email === 'philgw@gmail.com';
+
+  if (itsMe) {
+    console.log(`Submitting ${title} (${url})`);
+  } else {
+    console.log(`It wasn't me…`);
+  }
 
   return {
-    statusCode: 200,
+    statusCode: itsMe ? 200 : 403,
     body: JSON.stringify({
       identity: JSON.stringify(identity),
       user: JSON.stringify(user),
-      title: event.queryStringParameters.title,
-      url: event.queryStringParameters.url,
+      title,
+      url,
     }),
   };
 };
