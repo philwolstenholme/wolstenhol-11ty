f1f0f6edf0923a436a9730ed4bdd6711e83403f5
Phil Wolstenholme <philgw@gmail.com>
2022-03-05 19:55:39 +0000

    Don't query Spotify/Pipedream is the tab is not visible

--- src/assets/scripts/main.js
+++ src/assets/scripts/main.js
@@ -370,6 +370,11 @@ Alpine.data('PwSpotifyLive', () => ({
   queryInterval: null,
   queryTimeout: null,
   queryApi() {
+    if (document.visibilityState !== 'visible') {
+      console.log(`🔕 Tab not visible so we didn't query Spotify`);
+      return;
+    }
+
     console.log(`🎵 Checking the Spotify API via Pipedream…`);
     fetch('https://wolstenhol.me/api/recently-played-spotify')
       .then(res => res.json())
@@ -406,6 +411,11 @@ Alpine.data('PwSpotifyLive', () => ({
     clearInterval(this.queryInterval);
     clearTimeout(this.queryTimeout);
   },
+  onVisibilityChange() {
+    if (document.visibilityState === 'visible') {
+      this.queryApi();
+    }
+  },
   init() {
     this.queryApi();
 
@@ -420,10 +430,13 @@ Alpine.data('PwSpotifyLive', () => ({
         timeago.render(labelEl);
       });
     });
+
+    document.addEventListener('visibilitychange', this.onVisibilityChange.bind(this));
   },
   destroy() {
     clearInterval(this.queryInterval);
     clearTimeout(this.queryTimeout);
+    document.removeEventListener('visibilitychange', this.onVisibilityChange);
   },
 }));
 
@@ -457,4 +470,3 @@ console.info(
     text-shadow: rgb(0, 0, 0) 3px 0px 0px, rgb(0, 0, 0) 2.83487px 0.981584px 0px, rgb(0, 0, 0) 2.35766px 1.85511px 0px, rgb(0, 0, 0) 1.62091px 2.52441px 0px, rgb(0, 0, 0) 0.705713px 2.91581px 0px, rgb(0, 0, 0) -0.287171px 2.98622px 0px, rgb(0, 0, 0) -1.24844px 2.72789px 0px, rgb(0, 0, 0) -2.07227px 2.16926px 0px, rgb(0, 0, 0) -2.66798px 1.37182px 0px, rgb(0, 0, 0) -2.96998px 0.42336px 0px, rgb(0, 0, 0) -2.94502px -0.571704px 0px, rgb(0, 0, 0) -2.59586px -1.50383px 0px, rgb(0, 0, 0) -1.96093px -2.27041px 0px, rgb(0, 0, 0) -1.11013px -2.78704px 0px, rgb(0, 0, 0) -0.137119px -2.99686px 0px, rgb(0, 0, 0) 0.850987px -2.87677px 0px, rgb(0, 0, 0) 1.74541px -2.43999px 0px, rgb(0, 0, 0) 2.44769px -1.73459px 0px, rgb(0, 0, 0) 2.88051px -0.838247px 0px;
 `
 );
-console.info('%c🔗 🌍 https://github.com/philwolstenholme/wolstenhol-11ty/issues/new 🌍 🔗', `font-size: 1.5em;`);
