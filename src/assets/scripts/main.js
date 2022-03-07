63db3c74c698cff5c99e5c423a96f5916e464e6e
Phil Wolstenholme <philgw@gmail.com>
2022-03-07 13:43:51 +0000

    Add comments to SpotifyLive Alpine component

--- src/assets/scripts/main.js
+++ src/assets/scripts/main.js
@@ -380,9 +380,11 @@ Alpine.data('PwSpotifyLive', () => ({
       .then(res => res.json())
       .then(res => (this.data = res))
       .then(() => {
+        // If loadjs has't previously loaded timeago then let's load it.
         if (!loadjs.isDefined('timeago')) {
           loadjs('https://cdn.jsdelivr.net/npm/timeago.js@4.0.2/dist/timeago.min.js', 'timeago', {
             before: (path, el) => {
+              // We add a SRI hash to make up for the security risk of loading JS from a third-party.
               el.integrity = 'sha256-sTurDi2etLN9CpnUIoCC9y5iynb2qr/uo6QJqzoO7mA=';
               el.crossOrigin = 'anonymous';
             },
@@ -390,7 +392,10 @@ Alpine.data('PwSpotifyLive', () => ({
         }
 
         loadjs.ready('timeago', () => {
+          // Apply timeago against the DOM node for our 'x minutes ago' label.
           timeago.render(this.$refs.label);
+          // Set a boolean so we can display our component. We do this to avoid a flash of content
+          // jumping in once timeago has been loaded and applied to the element.
           this.timeagoVisible = true;
         });
       })
@@ -399,7 +404,10 @@ Alpine.data('PwSpotifyLive', () => ({
   startInterval() {
     const timeoutInMinutes = 15;
     console.log(`🔈 Starting to check the Spotify API every so often to see what I might be listening to`);
+    // Create an interval to check the API every-so-often.
     this.queryInterval = setInterval(this.queryApi.bind(this), MILLISECONDS_IN_A_SECOND * 30);
+    // Create a timeout to eventually clear the interval so we don't hit the API infinitely if someone
+    // leaves the page open forever.
     this.queryTimeout = setTimeout(() => {
       clearInterval(this.queryInterval);
     }, MILLISECONDS_IN_A_SECOND * SECONDS_IN_A_MINUTE * timeoutInMinutes);
@@ -412,14 +420,18 @@ Alpine.data('PwSpotifyLive', () => ({
     clearTimeout(this.queryTimeout);
   },
   onVisibilityChange() {
+    // If someone has just come back to the page after having it hidden then let's hit the API.
     if (document.visibilityState === 'visible') {
       this.queryApi();
     }
   },
   init() {
+    // Hit the API when the component is initalised.
     this.queryApi();
 
     loadjs.ready('timeago', () => {
+      // If the playedAt time from the API changes (e.g. a new track starts playing) then we need
+      // to reinitalise timeago, cancelling any existing instance and then creating a new one.
       this.$watch('data.playedAt', () => {
         const labelEl = this.$refs.label;
 
