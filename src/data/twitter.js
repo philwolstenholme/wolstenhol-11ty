63d3359888b8690a20b53ac0fbc864464ed75291
Phil Wolstenholme <philgw@gmail.com>
2022-05-19 21:28:06 +0100

    Show fewer tweets

--- src/data/twitter.js
+++ src/data/twitter.js
@@ -3,7 +3,7 @@ const Cache = require('@11ty/eleventy-cache-assets');
 
 module.exports = async function () {
   let response = await Cache(
-    'https://api.twitter.com/1.1/statuses/user_timeline.json?screen_name=philw_&include_rts=true&exclude_replies=true&tweet_mode=extended&count=60&include_ext_alt_text=true',
+    'https://api.twitter.com/1.1/statuses/user_timeline.json?screen_name=philw_&include_rts=true&exclude_replies=true&tweet_mode=extended&count=30&include_ext_alt_text=true',
     {
       duration: '1h',
       type: 'json',
