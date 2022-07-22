df638c848093c146ee22948752809637461174d5
Phil Wolstenholme <philgw@gmail.com>
2022-07-22 20:12:29 +0100

    Authenticate with Github API

--- src/data/githubStars.js
+++ src/data/githubStars.js
@@ -5,6 +5,11 @@ module.exports = async function () {
   let response = await Cache('https://api.github.com/users/philwolstenholme/starred', {
     duration: '1h',
     type: 'json',
+    fetchOptions: {
+      headers: {
+        Authorization: `token ${process.env.GITHUB_PAT}`,
+      },
+    },
   });
 
   return _.shuffle(response).slice(0, 12);
