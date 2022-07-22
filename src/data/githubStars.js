efe0ccf9bfc100860de4ab67984eb2a66cba06d6
Phil Wolstenholme <philgw@gmail.com>
2022-07-22 20:31:31 +0100

    Increase pool of starred repo data from the default (30) to 50

--- src/data/githubStars.js
+++ src/data/githubStars.js
@@ -2,7 +2,7 @@ const Cache = require('@11ty/eleventy-cache-assets');
 const _ = require('lodash');
 
 module.exports = async function () {
-  let response = await Cache('https://api.github.com/users/philwolstenholme/starred', {
+  let response = await Cache('https://api.github.com/users/philwolstenholme/starred?per_page=50', {
     duration: '1h',
     type: 'json',
     fetchOptions: {
