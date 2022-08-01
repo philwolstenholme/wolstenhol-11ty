9b5e6d359af4b7fade3dcb6ad34f5eee97484f44
Phil Wolstenholme <philgw@gmail.com>
2022-08-01 19:50:37 +0100

    Add SRI hashes for everything loaded from jsDelivr

--- src/assets/scripts/main.js
+++ src/assets/scripts/main.js
@@ -265,15 +265,22 @@ document.addEventListener('alpine:init', () => {
     extraTweets: null,
     colcade() {
       if (window.innerWidth > 767 && this.colcadeInstance === null) {
-        loadjs('https://wolstenhol.me/proxy/jsdelivr/npm/colcade@0.2.0/colcade.js', () => {
-          try {
-            this.colcadeInstance = new Colcade('.tweets-grid', {
-              columns: '.tweets-grid__col',
-              items: '.tweets-grid__item',
-            });
-          } catch (error) {
-            console.error(`Colcade is being weird again…`, error);
-          }
+        loadjs('https://wolstenhol.me/proxy/jsdelivr/npm/colcade@0.2.0/colcade.js', 'colcade', {
+          success: () => {
+            try {
+              this.colcadeInstance = new Colcade('.tweets-grid', {
+                columns: '.tweets-grid__col',
+                items: '.tweets-grid__item',
+              });
+            } catch (error) {
+              console.error(`Colcade is being weird again…`, error);
+            }
+          },
+          before: (path, el) => {
+            // We add a SRI hash to make up for the security risk of loading JS from a third-party.
+            el.integrity = 'sha256-ZxEJSCFR4d0OThzWuZ8CYCzw+pDoV/E0/+4EWoLO6Eg=';
+            el.crossOrigin = 'anonymous';
+          },
         });
       }
     },
@@ -373,54 +380,61 @@ document.addEventListener('alpine:init', () => {
   Alpine.data('PwCardInstagram', $root => ({
     playing: false,
     confetti($root) {
-      loadjs('https://wolstenhol.me/proxy/jsdelivr/npm/canvas-confetti@1.4.0/dist/confetti.browser.min.js', function () {
-        var dimensions = $root.getBoundingClientRect();
-        var centerXCoord = dimensions.left + window.pageXOffset + dimensions.width / 2;
-        var centerYCoord = dimensions.y + dimensions.height / 2;
+      loadjs('https://cdn.jsdelivr.net/npm/canvas-confetti@1.5.1/dist/confetti.browser.js', 'canvas-confetti', {
+        success: () => {
+          var dimensions = $root.getBoundingClientRect();
+          var centerXCoord = dimensions.left + window.pageXOffset + dimensions.width / 2;
+          var centerYCoord = dimensions.y + dimensions.height / 2;
 
-        var count = 150;
-        var defaults = {
-          origin: {
-            y: centerYCoord / window.innerHeight,
-            x: centerXCoord / window.innerWidth,
-          },
-          disableForReducedMotion: true,
-        };
+          var count = 150;
+          var defaults = {
+            origin: {
+              y: centerYCoord / window.innerHeight,
+              x: centerXCoord / window.innerWidth,
+            },
+            disableForReducedMotion: true,
+          };
 
-        function fire(particleRatio, opts) {
-          confetti(
-            Object.assign({}, defaults, opts, {
-              particleCount: Math.floor(count * particleRatio),
-            })
-          );
-        }
+          function fire(particleRatio, opts) {
+            confetti(
+              Object.assign({}, defaults, opts, {
+                particleCount: Math.floor(count * particleRatio),
+              })
+            );
+          }
 
-        fire(0.25, {
-          spread: 26,
-          startVelocity: 55,
-        });
+          fire(0.25, {
+            spread: 26,
+            startVelocity: 55,
+          });
 
-        fire(0.2, {
-          spread: 60,
-        });
+          fire(0.2, {
+            spread: 60,
+          });
 
-        fire(0.35, {
-          spread: 100,
-          decay: 0.91,
-          scalar: 0.8,
-        });
+          fire(0.35, {
+            spread: 100,
+            decay: 0.91,
+            scalar: 0.8,
+          });
 
-        fire(0.1, {
-          spread: 120,
-          startVelocity: 25,
-          decay: 0.92,
-          scalar: 1.2,
-        });
+          fire(0.1, {
+            spread: 120,
+            startVelocity: 25,
+            decay: 0.92,
+            scalar: 1.2,
+          });
 
-        fire(0.1, {
-          spread: 120,
-          startVelocity: 45,
-        });
+          fire(0.1, {
+            spread: 120,
+            startVelocity: 45,
+          });
+        },
+        before: (path, el) => {
+          // We add a SRI hash to make up for the security risk of loading JS from a third-party.
+          el.integrity = 'sha256-DLqQ9Jy1MLOv8QhzYyGo2km2ZBmOZFsL2GsXlogfgOk=';
+          el.crossOrigin = 'anonymous';
+        },
       });
     },
   }));
