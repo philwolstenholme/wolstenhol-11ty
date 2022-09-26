c69e0806e50345aa86dea7b94068482d16e58a4a
Phil Wolstenholme <philgw@gmail.com>
2022-09-26 23:36:11 +0100

    Move no-js functionality to a Netlify Edge Function

---
+++ netlify/edge-functions/no-js.js
@@ -0,0 +1,139 @@
+import { HTMLRewriter } from 'https://ghuc.cc/worker-tools/html-rewriter/index.ts';
+
+class ScriptRemover {
+  element(element) {
+    element.remove();
+  }
+}
+
+class JSAttributeRemover {
+  element(element) {
+    const globalEventHandlers = [
+      'onabort',
+      'onanimationcancel',
+      'onanimationend',
+      'onanimationiteration',
+      'onauxclick',
+      'onblur',
+      'oncancel',
+      'oncanplay',
+      'oncanplaythrough',
+      'onchange',
+      'onclick',
+      'onclose',
+      'oncontextmenu',
+      'oncuechange',
+      'ondblclick',
+      'ondrag',
+      'ondragend',
+      'ondragenter',
+      'ondragexit',
+      'ondragleave',
+      'ondragover',
+      'ondragstart',
+      'ondrop',
+      'ondurationchange',
+      'onemptied',
+      'onended',
+      'onerror',
+      'onfocus',
+      'ongotpointercapture',
+      'oninput',
+      'oninvalid',
+      'onkeydown',
+      'onkeypress',
+      'onkeyup',
+      'onload',
+      'onloadeddata',
+      'onloadedmetadata',
+      'onloadstart',
+      'onlostpointercapture',
+      'onmousedown',
+      'onmouseenter',
+      'onmouseleave',
+      'onmousemove',
+      'onmouseout',
+      'onmouseover',
+      'onmouseup',
+      'onpause',
+      'onplay',
+      'onplaying',
+      'onpointercancel',
+      'onpointerdown',
+      'onpointerenter',
+      'onpointerleave',
+      'onpointermove',
+      'onpointerout',
+      'onpointerover',
+      'onpointerup',
+      'onprogress',
+      'onratechange',
+      'onreset',
+      'onresize',
+      'onscroll',
+      'onsecuritypolicyviolation',
+      'onseeked',
+      'onseeking',
+      'onselect',
+      'onselectionchange',
+      'onselectstart',
+      'onstalled',
+      'onsubmit',
+      'onsuspend',
+      'ontimeupdate',
+      'ontoggle',
+      'ontouchcancel',
+      'ontouchend',
+      'ontouchmove',
+      'ontouchstart',
+      'ontransitioncancel',
+      'ontransitionend',
+      'onvolumechange',
+      'onwaiting',
+      'onwheel',
+    ];
+
+    const attributesToRemove = ['x-data', 'x-init', ...globalEventHandlers];
+
+    attributesToRemove.forEach(attribute => {
+      if (element.hasAttribute(attribute)) {
+        element.removeAttribute(attribute);
+      }
+    });
+  }
+}
+
+class NoScriptContentUnwrapper {
+  element(element) {
+    element.removeAndKeepContent();
+  }
+}
+
+class TitleRewriter {
+  element(element) {
+    element.prepend('[JS automatically removed] ');
+  }
+}
+
+export default async (request, context) => {
+  const url = new URL(request.url);
+  if (url.searchParams.get('noJs') !== 'true') {
+    return;
+  }
+
+  context.log('Rustling up a no-JS version');
+
+  const response = await context.next();
+
+  const transformedResponse = new HTMLRewriter()
+    .on('script', new ScriptRemover())
+    .on('noscript', new NoScriptContentUnwrapper())
+    .on('title', new TitleRewriter())
+    .on('*', new JSAttributeRemover())
+    .transform(response);
+
+  transformedResponse.headers.set('X-Robots-Tag', 'noindex');
+  transformedResponse.headers.set('Link', `<${url.href.replace(url.search, '')}>; rel="canonical"`);
+
+  return transformedResponse;
+};
--- src/data/eleventyComputed.js
+++ src/data/eleventyComputed.js
@@ -6,18 +6,4 @@ module.exports = {
       return `https://wolstenhol.me/no-css${data.page.filePathStem}`;
     }
   },
-  noJsUrl: data => {
-    if (data.page.filePathStem == '/index') {
-      return `https://wolstenhol.me/no-js`;
-    } else {
-      return `https://wolstenhol.me/no-js${data.page.filePathStem}`;
-    }
-  },
-  fullFatUrl: data => {
-    if (data.page.filePathStem == '/index') {
-      return '/';
-    } else {
-      return data.page.filePathStem;
-    }
-  },
 };
