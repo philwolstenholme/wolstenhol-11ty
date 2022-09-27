ff0c1d2ef7fb2192f391cc7ac655fddcafb29633
Phil Wolstenholme <philgw@gmail.com>
2022-09-28 00:09:43 +0100

    Add in some extra html-only headers from simonhearne

--- netlify/edge-functions/strip-non-html-headers.js
+++ netlify/edge-functions/strip-non-html-headers.js
@@ -8,20 +8,28 @@ export default async (request, context) => {
   }
 
   const htmlOnlyHeaders = [
+    'x-frame-options',
+    // https://webhint.io/docs/user-guide/hints/hint-no-html-only-headers
     'content-security-policy',
     'x-content-security-policy',
     'x-ua-compatible',
     'x-webkit-csp',
     'x-xss-protection',
-    'x-frame-options',
-    // https://webhint.io/docs/user-guide/hints/hint-no-html-only-headers
+    // https://gist.github.com/simonhearne/2715250eaccb911ebfff9e2315b2ffee
+    'content-security-policy-report-only',
+    'feature-policy',
+    'nel',
+    'report-to',
+    'accept-ch',
+    'referrer-policy',
+    'strict-transport-security',
+    'link',
   ];
 
   response.headers.forEach((value, key, object) => {
     if (contentType.startsWith('text/javascript') && (key === 'content-security-policy' || key === 'x-content-security-policy')) {
       // In case of a JavaScript file, Content-Security-Policy and X-Content-Security-Policy
       // can be ignored since CSP is also relevant to workers.
-      context.log(`Ignoring as a JS file`);
       return;
     }
 
