f158698f57971f993b1e3d6b943088b151d8e4b0
Phil Wolstenholme <philgw@gmail.com>
2022-10-07 00:33:54 +0100

    Try and load Lozad a bit earlier

--- src/assets/scripts/main.js
+++ src/assets/scripts/main.js
@@ -8,6 +8,14 @@ import './utils/request-idle-callback.js';
 // import CssNakedDay from './css-naked-day.js';
 // CssNakedDay();
 
+requestIdleCallback(async () => {
+  const { default: lozad } = await import('lozad');
+  window.lozad = lozad('[data-lozad]', {
+    enableAutoReload: true,
+  });
+  window.lozad.observe();
+});
+
 window.Alpine = Alpine;
 
 Alpine.plugin(intersect);
@@ -53,14 +61,6 @@ console.info(
     text-shadow: rgb(0, 0, 0) 3px 0px 0px, rgb(0, 0, 0) 2.83487px 0.981584px 0px, rgb(0, 0, 0) 2.35766px 1.85511px 0px, rgb(0, 0, 0) 1.62091px 2.52441px 0px, rgb(0, 0, 0) 0.705713px 2.91581px 0px, rgb(0, 0, 0) -0.287171px 2.98622px 0px, rgb(0, 0, 0) -1.24844px 2.72789px 0px, rgb(0, 0, 0) -2.07227px 2.16926px 0px, rgb(0, 0, 0) -2.66798px 1.37182px 0px, rgb(0, 0, 0) -2.96998px 0.42336px 0px, rgb(0, 0, 0) -2.94502px -0.571704px 0px, rgb(0, 0, 0) -2.59586px -1.50383px 0px, rgb(0, 0, 0) -1.96093px -2.27041px 0px, rgb(0, 0, 0) -1.11013px -2.78704px 0px, rgb(0, 0, 0) -0.137119px -2.99686px 0px, rgb(0, 0, 0) 0.850987px -2.87677px 0px, rgb(0, 0, 0) 1.74541px -2.43999px 0px, rgb(0, 0, 0) 2.44769px -1.73459px 0px, rgb(0, 0, 0) 2.88051px -0.838247px 0px;
 `
 );
-
-requestIdleCallback(async () => {
-  const { default: lozad } = await import('lozad');
-  window.lozad = lozad('[data-lozad]', {
-    enableAutoReload: true,
-  });
-  window.lozad.observe();
-});
 
 requestIdleCallback(async () => {
   loadjs('https://wolstenhol.me/proxy/jsdelivr/npm/instant.page@5.1.1/instantpage.js', 'instantPage', {
