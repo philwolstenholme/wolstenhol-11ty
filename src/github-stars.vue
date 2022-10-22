74c1484f7efdf312029b3b99cf2a8be4d98f720c
Phil Wolstenholme <philgw@gmail.com>
2022-10-22 13:19:41 +0100

    Update Github stars intro text

--- src/github-stars.vue
+++ src/github-stars.vue
@@ -3,11 +3,11 @@
     <h1 class="text-8xl bg-clip-text text-fill-transparent bg-binding-dark tracking-tight font-serif font-bold">{{ title }}</h1>
     <div class="font-serif my-10 space-y-5 text-2xl leading-normal">
       <p>
-        A randomised selection of projects that I've recently
+        A recent selection of projects that I've
         <a href="https://github.com/philwolstenholme?tab=stars" class="underline">starred on GitHub</a>.
       </p>
       <p>
-        This page is part for me to big up the work of great projects, and half to serve as a personal bookmark of repos I find interesting
+        This page is part for me to big up the work of great projects, and part to serve as a personal bookmark of repos I find interesting
         and might want to check out or use later.
       </p>
     </div>
