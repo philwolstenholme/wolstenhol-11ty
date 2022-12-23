b0de992a071be71117430abe9735a1aff9380699
Phil Wolstenholme <philgw@gmail.com>
2022-12-23 22:01:13 +0000

    Don't need the requestIdleCallback thingy as we await a role added via Alpine

--- tests/pw-music.spec.js
+++ tests/pw-music.spec.js
@@ -3,33 +3,9 @@ import { test, expect } from '@playwright/test';
 test('can play music then stop it with the esc key, and the title is updated', async ({ page }) => {
   await page.goto('https://wolstenhol.me/');
 
-  await page.evaluate(() => {
-    // polyfill requestIdleCallback if it doesn't exist on window
-    if (!window.requestIdleCallback) {
-      window.requestIdleCallback = cb => {
-        const start = Date.now();
-        return setTimeout(() => {
-          cb({
-            didTimeout: false,
-            timeRemaining: () => Math.max(0, 50 - (Date.now() - start)),
-          });
-        }, 1);
-      };
-    }
-  });
-
-  // use requestIdleCallback to wait for alpine-async to do its thing
-  await page.waitForFunction(() => {
-    return new Promise(resolve => {
-      requestIdleCallback(() => {
-        resolve(true);
-      });
-    });
-  });
-
   const firstMusicCard = await page.locator('[data-section="music"] ul > li:nth-child(1) > article');
   await firstMusicCard.scrollIntoViewIfNeeded();
-  const playButton = firstMusicCard.locator('a[role="button"]');
+  const playButton = await firstMusicCard.locator('a[role="button"]');
   const audioEl = await page.locator('audio');
   await playButton.click();
   await page.waitForFunction(() => document.title.includes('▶'));
