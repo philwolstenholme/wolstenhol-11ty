c855e4329174d6e031172a1fe627be9be5b5bea1
Phil Wolstenholme <philgw@gmail.com>
2022-12-23 15:10:00 +0000

    Temp fix to allow a build

--- src/data/webfinger.js
+++ src/data/webfinger.js
@@ -11,4 +11,13 @@ const getData = async function () {
   return response;
 };
 
-module.exports = tryForCache('webfinger', getData);
+module.exports = {
+  subject: 'acct:philw_@hachyderm.io',
+  aliases: ['https://hachyderm.io/@philw_', 'https://hachyderm.io/users/philw_', 'https://wolstenhol.me'],
+  links: [
+    { rel: 'http://webfinger.net/rel/profile-page', type: 'text/html', href: 'https://hachyderm.io/@philw_' },
+    { rel: 'self', type: 'application/activity+json', href: 'https://hachyderm.io/users/philw_' },
+    { rel: 'http://ostatus.org/schema/1.0/subscribe', template: 'https://hachyderm.io/authorize_interaction?uri={uri}' },
+    { rel: 'http://webfinger.net/rel/profile-page', type: 'text/html', href: 'https://wolstenhol.me' },
+  ],
+};
