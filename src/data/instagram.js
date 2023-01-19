e742d9e77aea7410226573b1380bac6987e1eac0
Phil Wolstenholme <philgw@gmail.com>
2023-01-19 23:18:26 +0000

    Swap to getting Instagram data from Gist

--- src/data/instagram.js
+++ src/data/instagram.js
@@ -1,69 +1,74 @@
+require('dotenv').config();
+const Cache = require('@11ty/eleventy-cache-assets');
 const cloudinary = require('cloudinary').v2;
 const fetchBase64 = require('fetch-base64');
 const svgToMiniDataURI = require('mini-svg-data-uri');
 const tryForCache = require('../../cache');
-const fs = require('fs');
 
 const getData = async function () {
-  const unprocessedPosts = JSON.parse(fs.readFileSync('./src/data/instagram.txt', 'utf8'));
+  let response = await Cache(
+    'https://gist.githubusercontent.com/philwolstenholme/0d8f663f0d5857d1e5d43aad021d9c7e/raw/fb7fad88d670d7fc86efbc798217492391127e97/instagram.json',
+    {
+      duration: '1h',
+      type: 'json',
+    }
+  );
 
-  unprocessedPosts.map(edge =>
+  response.data.user.edge_owner_to_timeline_media.edges.map(edge =>
     cloudinary.uploader.upload(edge.node.thumbnail_src, { tags: 'instagram', public_id: `11ty/instagram/${edge.node.id}` })
   );
 
-  const posts = unprocessedPosts.map(async post => {
+  const posts = response.data.user.edge_owner_to_timeline_media.edges.map(async edge => {
     const svgPlaceholder = await fetchBase64
-      .remote(`https://res.cloudinary.com/wolstenh/image/upload/f_auto,q_20,w_20/v1/11ty/instagram/${post.node.id}.jpg`)
+      .remote(`https://res.cloudinary.com/wolstenh/image/upload/f_auto,q_20,w_20/v1/11ty/instagram/${edge.node.id}.jpg`)
       .then(data => {
         let svg = `<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 0 50 50"><filter id="b" color-interpolation-filters="sRGB"><feGaussianBlur stdDeviation=".9"></feGaussianBlur><feComponentTransfer><feFuncA type="discrete" tableValues="1 1"></feFuncA></feComponentTransfer></filter><image filter="url(#b)" preserveAspectRatio="none" height="100%" width="100%" xlink:href="${data[1]}"></image></svg>`;
 
         return svgToMiniDataURI(svg);
       })
-      .catch(reason => {
-        console.error(reason);
-      });
+      .catch(reason => {});
 
-    if (post.node.edge_sidecar_to_children) {
-      const firstChild = post.node.edge_sidecar_to_children.edges[0].node;
+    if (edge.node.edge_sidecar_to_children) {
+      const firstChild = edge.node.edge_sidecar_to_children.edges[0].node;
       const type = firstChild['__typename'];
 
       if (type === 'GraphVideo') {
-        post.node.is_video = firstChild.is_video;
-        post.node.video_url = firstChild.video_url;
+        edge.node.is_video = firstChild.is_video;
+        edge.node.video_url = firstChild.video_url;
       }
     }
 
     return {
-      id: post.node.id,
+      id: edge.node.id,
       images: {
         standard_resolution: {
-          url: post.node.thumbnail_src,
+          url: edge.node.thumbnail_src,
         },
       },
-      link: `https://instagram.com/p/${post.node.shortcode}`,
+      link: `https://instagram.com/p/${edge.node.shortcode}`,
       caption: {
-        text: post.node.edge_media_to_caption.edges.length > 0 ? post.node.edge_media_to_caption.edges[0].node.text : null,
+        text: edge.node.edge_media_to_caption.edges.length > 0 ? edge.node.edge_media_to_caption.edges[0].node.text : null,
       },
       likes: {
-        count: post.node.edge_media_preview_like.count,
+        count: edge.node.edge_media_preview_like.count,
       },
       comments: {
-        count: post.node.edge_media_to_comment.count,
+        count: edge.node.edge_media_to_comment.count,
       },
       location: {
-        name: post.node.location ? post.node.location.name : null,
+        name: edge.node.location ? edge.node.location.name : null,
       },
-      videos: post.node.is_video
+      videos: edge.node.is_video
         ? {
             standard_resolution: {
-              url: post.node.video_url,
+              url: edge.node.video_url,
             },
           }
         : null,
-      accessibilityCaption: post.node.accessibility_caption,
+      accessibilityCaption: edge.node.accessibility_caption,
       svgPlaceholder: svgPlaceholder,
-      display_url: post.node.display_url,
-      dimensions: post.node.dimensions,
+      display_url: edge.node.display_url,
+      dimensions: edge.node.dimensions,
     };
   });
 
