674e4ae670045f55f1d974bad430c470d8490ef4
Phil Wolstenholme <philgw@gmail.com>
2023-02-11 23:44:47 +0000

    Get ready for Twitter API to be pulled

--- src/data/twitter.js
+++ src/data/twitter.js
@@ -3,18 +3,24 @@ const Cache = require('@11ty/eleventy-cache-assets');
 const tryForCache = require('../../cache');
 
 const getData = async function () {
-  let response = await Cache(
-    'https://api.twitter.com/1.1/statuses/user_timeline.json?screen_name=philw_&include_rts=true&exclude_replies=true&tweet_mode=extended&count=60&include_ext_alt_text=true',
-    {
-      duration: '30m',
-      type: 'json',
-      fetchOptions: {
-        headers: {
-          Authorization: process.env.TWITTER_AUTH_BEARER,
+  let response;
+  try {
+    response = await Cache(
+      'https://api.twitter.com/1.1/statuses/user_timeline.json?screen_name=philw_&include_rts=true&exclude_replies=true&tweet_mode=extended&count=60&include_ext_alt_text=true',
+      {
+        duration: '30m',
+        type: 'json',
+        fetchOptions: {
+          headers: {
+            Authorization: process.env.TWITTER_AUTH_BEARER,
+          },
         },
-      },
-    }
-  );
+      }
+    );
+  } catch (error) {
+    console.log(error);
+    return [];
+  }
 
   response = response.filter(entry => !entry.full_text.startsWith('📚 Added'));
 
